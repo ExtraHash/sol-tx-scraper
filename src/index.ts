95ffe9a633d8e65093e166381ed12c46a8e1d3b6
ExtraHash <ExtraHash@protonmail.com>
2021-12-15 07:49:12 +0000

    clean up a bit further

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,39 @@
 import log from "electron-log";
-import { Connection } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { sleep } from "./utils/sleep";
 import { SOL_RPC_URI } from "./constants/env";
-import { exchangeWallets } from "./constants/exchangeDetails";
-import { ExchangeDetails } from "./types";
 import sizeof from "object-sizeof";
 
 // set up the sol rpc
 const connection = new Connection(SOL_RPC_URI);
 
+// addresses to scrape
+const addresses = ["cndyAnrLdpjq1Ssp1z8xxDsB8dxe7u4HL5Nxi2K5WXZ"];
+
 // program entry
 async function main() {
     const scrapers = [];
-    for (const exchange of exchangeWallets) {
-        scrapers.push(monitorTransactions(exchange));
+    for (const address of addresses) {
+        scrapers.push(monitorTransactions(new PublicKey(address)));
     }
     // this will never resolve
     await Promise.all(scrapers);
 }
 
 // monitor transactions loop
-async function monitorTransactions(exchange: ExchangeDetails) {
+async function monitorTransactions(address: PublicKey) {
     // this is the last known signature
     let lastSeen: string | undefined;
 
-    log.info(
-        "Fetching transactions every five seconds from " + exchange.address
-    );
+    log.info("Fetching transactions every five seconds from " + address);
 
     while (true) {
         try {
             /* fetch signatures more recent than the last one known
             the first fetch will fetch 1000 transactions */
-            const res: any = await connection.getSignaturesForAddress(
-                exchange.address,
-                {
-                    until: lastSeen,
-                }
-            );
+            const res: any = await connection.getSignaturesForAddress(address, {
+                until: lastSeen,
+            });
 
             // sometimes it returns no signatures (empty array)
             if (res.length > 0) {
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-
-export interface ExchangeDetails {
-    name: string;
-    address: PublicKey;
-}
